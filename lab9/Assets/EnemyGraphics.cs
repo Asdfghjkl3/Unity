c223d583fa320ade2bd6bb1560d2a7ae7fae7321
hours3335 <hours3335@gmail.com>
2021-12-30 14:36:57 +0100

    Add lab8, lab9, lab10

---
+++ lab9/Assets/EnemyGraphics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGraphics : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0f);
+        }
+        else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0f);
+        }
+    }
+}
---
+++ lab9/Assets/Scripts/EnemyPathScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathScript : MonoBehaviour
+{
+    public GameObject player;
+    public CharacterController2D controller;
+    public float speed = 17f;
+    public Animator animator;
+    public float startingPoint;
+    public float endingPoint;
+    bool inEnemysRangeX = false;
+    bool inEnemysRangeY = false;
+    bool inPathStart = false;
+    bool inPathEnd = false;
+
+
+    void Update()
+    {
+        inEnemysRangeX = (player.transform.position.x >= startingPoint && player.transform.position.x <= endingPoint) ? true : false;
+        inEnemysRangeY = (player.transform.position.y >= transform.position.y - 0.1f && player.transform.position.y <= transform.position.y + 3f) ? true : false;
+        inPathStart = (transform.position.x < startingPoint && speed < 0) ? true : false;
+        inPathEnd = (transform.position.x > endingPoint && speed > 0) ? true : false;
+
+
+        //Debug.Log(inEnemysRangeX);
+        //Debug.Log(inEnemysRangeY);
+
+        if (inEnemysRangeX && inEnemysRangeY)
+        {
+            speed = (player.transform.position.x < transform.position.x && speed > 0) ? speed *= -1 : speed;
+            speed = (player.transform.position.x > transform.position.x && speed < 0) ? speed *= -1 : speed;
+        }
+        else if (inPathStart || inPathEnd)
+        {
+            speed *= -1;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        controller.Move(speed * Time.deltaTime, false);
+
+
+    }
+}
